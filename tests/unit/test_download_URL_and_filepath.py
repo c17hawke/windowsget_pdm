b7ff4fdcaa35c4b7ab9d04d0b0bd4e7fdaf14fc6
c17hawke <sunny.c17hawke@gmail.com>
2023-03-27 16:04:00 +0530

    first commit

---
+++ tests/unit/test_download_URL_and_filepath.py
@@ -0,0 +1,37 @@
+from windowsget_pdm import download
+import pytest
+from pathlib import Path
+import shutil
+
+
+
+
+correct_filepaths = [
+    ("https://raw.githubusercontent.com/c17hawke/raw_data/main/interactions.csv", "x/y/test.csv"),
+    ("https://raw.githubusercontent.com/c17hawke/raw_data/main/interactions.csv", "example.csv"),
+] 
+
+bad_filepaths = [
+    ("https://raw.githubusercontent.com/c17hawke/raw_data/main/interactions.csv", "x/y/test"),
+    ("https://raw.githubusercontent.com/c17hawke/raw_data/main/interactions.csv", "example"),
+]
+
+
+@pytest.mark.parametrize("url, filepath", correct_filepaths)
+def test_correct_url_and_filepath(url, filepath):
+    TEST_DIR = "test_dir"
+    test_filepath = f"{TEST_DIR}/{filepath}"
+    response = download(url=url, filepath=Path(test_filepath))
+    assert isinstance(response, int)
+    assert Path(test_filepath).exists()
+    shutil.rmtree(TEST_DIR)
+
+
+@pytest.mark.parametrize("url, filepath", bad_filepaths)
+def test_bad_url_and_filepath(url, filepath):
+    TEST_DIR = "test_dir"
+    test_filepath = f"{TEST_DIR}/{filepath}"
+    with pytest.raises(ValueError):
+        response = download(url=url, filepath=Path(test_filepath))
+    shutil.rmtree(TEST_DIR)
+
---
+++ tests/unit/test_download_URL_only.py
@@ -0,0 +1,27 @@
+from windowsget_pdm import download
+import pytest
+from pathlib import Path
+from requests.exceptions import HTTPError
+import os
+
+correct_urls = [
+    "https://raw.githubusercontent.com/c17hawke/raw_data/main/interactions.csv"
+] 
+
+bad_urls = [
+    "https://raw.githubusercontent.com/c17hawke/raw_data/main/interactions",
+    "https://raw.githubusercontent.com/c17hawke/raw_data/main/xyz"
+]
+
+@pytest.mark.parametrize("url", correct_urls)
+def test_correct_urls_only(url):
+    filepath = Path(url).name
+    response = download(url=url, filepath=Path(filepath))
+    assert isinstance(response, int)
+    os.remove(Path(filepath))
+
+@pytest.mark.parametrize("url", bad_urls)
+def test_bad_urls_only(url):
+    filepath = Path(url).name
+    with pytest.raises(HTTPError):
+        response = download(url=url, filepath=Path(filepath))
